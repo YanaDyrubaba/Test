b470b827e28dc5a1b4bb834cd85760e55d565562
Yana <yanusya33@gmail.com>
2016-08-26 16:57:45 +0300

    csdcxsx

--- Test/src/ua/Main.java
+++ Test/src/ua/Main.java
@@ -3,6 +3,10 @@ package ua;
 public class Main {
 
 	public static void main(String[] args) {
+		print();
+	}
+	
+	static void print(){
 		System.out.println("Hello");
 		
 		System.out.println("World");
